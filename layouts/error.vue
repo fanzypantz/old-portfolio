18998bfe362139031c4c1805acfad67ec7ab6908
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-06-14 22:10:50 +0200

    added error layout and error page

---
+++ layouts/error.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1>Error {{ error.statusCode }}</h1>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Go Home</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  layout: 'errorLayout'
+};
+</script>
+
+<style lang="sass">
+body
+  min-height: 100vh !important
+
+
+.container
+  height: 100vh
+  width: 100vw
+  display: flex
+  justify-content: center
+  align-items: center
+  flex-direction: column
+</style>
---
+++ layouts/errorLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main-container">
+    <nuxt @click.self="closeMenu" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass"></style>
